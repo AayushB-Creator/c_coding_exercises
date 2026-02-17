28c6d5512ceb24c9438cbf540f5cfec631534379
Aayush Bharti <aayushb22.cse@gmail.com>
2026-02-17 18:37:15 +0530

    four.c

--- four.c
+++ four.c
@@ -11,7 +11,7 @@ int main() {
     for(int i=0;address[i]!='\0';i++){
         if(address[i]==',') {
             printf("\n");
-            i++;
+            if(address[i+1]==' ') i++; //skips the space just after comma
         }
         else printf("%c",address[i]);
     }
