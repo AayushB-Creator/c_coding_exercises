ab5a22daca3f6c79bb3ea7900e5b533abb81d13b
Aayush Bharti <aayushb22.cse@gmail.com>
2026-02-17 19:56:51 +0530

    thirteen.c

--- thirteen.c
+++ thirteen.c
@@ -5,10 +5,16 @@ using fxns
 #include <stdio.h>
 int smallest3(int n1,int n2,int n3){
     int smallest;
-    if(n1<=n2 && n1<=n3) smallest=n1;
+    if(n1<=n2 && n1<=n3) smallest=n1; //compare 1 no. independently with others
     else if(n1>=n2 && n2<=n3) smallest=n2;
     else smallest=n3;
     return smallest;
+    /*
+    smallest = n1
+    if(n2<smallest) smallest=n2
+    if(n3<smallest) smallest=n3
+    this is even better!
+    */
 }
 int main(){
     int a,b,c;
