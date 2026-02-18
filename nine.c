c6e373695548ed58138002d996ee04eaf20260ab
Aayush Bharti <aayushb22.cse@gmail.com>
2026-02-18 19:10:06 +0530

    nine.c

--- nine.c
+++ nine.c
@@ -12,4 +12,25 @@ then display the bill in the format as :
     Total amount to be paid :   ₹XXXX
 ---------------------------------------------
 */
-
+#include <stdio.h>
+int main(){
+    printf("enter item :");
+    char item[20];
+    scanf("%s",item);
+    printf("enter qty :");
+    int qty;
+    scanf("%d",&qty);
+    printf("enter price :");
+    float price;
+    scanf("%f",&price);
+    float total=0;
+    total=total+qty*price;
+    printf("***************** B I L L ******************\n");
+    printf("    Item    Quantity    Price   Amount\n");
+    printf("---------------------------------------------\n");
+    printf("    %s   %d        %.2f   %.2f\n",item,qty,price,qty*price);
+    printf("---------------------------------------------\n");
+    printf("    Total amount to be paid :   ₹%.2f\n",total);
+    printf("---------------------------------------------\n");
+    return 0;
+}
