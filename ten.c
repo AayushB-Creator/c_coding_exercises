eb3d3839ddf09c2dee4383984f9637164c95c13c
Aayush Bharti <aayushb22.cse@gmail.com>
2026-02-18 18:51:07 +0530

    ten.c

---
+++ ten.c
@@ -0,0 +1,11 @@
+/*
+wap using print to print following BYE pattern
+*/
+#include <stdio.h>
+int main(){
+    printf("BBB     Y       Y       EEEE\n");
+    printf("B   B    Y    Y         E\n");
+    printf("BBB         Y           EEEE\n");
+    printf("B   B       Y           E\n");
+    return 0;
+}
